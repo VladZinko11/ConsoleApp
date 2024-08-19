4acec365cbd269e3059665f1eb4a316a636018f4
VladZinko <zinko1996@yandex.ru>
2024-08-19 18:33:49 +0300

    modified method main

--- src/main/java/com/zinko/Main.java
+++ src/main/java/com/zinko/Main.java
@@ -1,12 +1,14 @@
 package com.zinko;
 
 import com.zinko.command.CommandListener;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        CommandListener commandListener = new CommandListener();
-        commandListener.handleCommand();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConsoleAppContext.class);
+        CommandListener commandListener = context.getBean("commandListener", CommandListener.class);
+        commandListener.ListenCommand();
     }
 }
