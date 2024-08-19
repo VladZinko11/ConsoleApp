766d7a4e0734fa6313f0648a658a777228da07c0
VladZinko <zinko1996@yandex.ru>
2024-08-19 15:00:24 +0300

    created class UserIdGenerator

---
+++ src/main/java/com/zinko/data/UserIdGenerator.java
@@ -0,0 +1,33 @@
+package com.zinko.data;
+
+import com.zinko.service.exception.ConsoleAppException;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.io.input.ReversedLinesFileReader;
+
+import java.io.File;
+import java.io.IOException;
+
+
+@Getter
+@Setter
+public class UserIdGenerator {
+    private static Long lastExistId;
+
+
+    static {
+        File fileReader = new File("File.txt");
+        try (ReversedLinesFileReader reader = new ReversedLinesFileReader(fileReader)) {
+            String line = reader.readLine();
+            String[] split = line.split(" \\| ", 2);
+            lastExistId = Long.parseLong(split[0]);
+        } catch (IOException e) {
+            throw new ConsoleAppException("I/O error occurs");
+        }
+    }
+
+    public static Long getNewUserId() {
+        lastExistId++;
+        return lastExistId;
+    }
+}
